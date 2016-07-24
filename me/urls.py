91ea5831f9d0875a29968d36ed35a4756725af0c
Garanin Pavel <mur069@yandex.ru>
2016-07-24 13:53:23 -0400

    init all menu

--- me/urls.py
+++ me/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from .views import index 
+from .views import index,projects,about_me,contact
 
 urlpatterns = [
     url(r'^$', index ),
+    url(r'projects/$', projects ),
+    url(r'about/$', about_me),
+    url(r'contact/$', contact ),
 ]
--- me/views.py
+++ me/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render,render_to_response
 from django.http.response import HttpResponse
 from django.template.loader import get_template
-from .models import Persona, Skill, Project
+from .models import Persona, Skill, Project,Article
 
 # def last_news(count=1):
 #     return Project.objects.all()
 
 def index(request):
-    return render_to_response('home_page/home.html',{'menu':1})
+    return render_to_response('home_page/home.html',{'page_menu':1})
 
 def projects(request):
-    return render_to_response('projects/projects.html',{'menu':2})
+    return render_to_response('projects/projects.html',{'page_menu':2})
 
-def aboutMe(request):
+def contact(reques):
+    return render_to_response('contact/contact.html',{'page_menu':4})
+
+def about_me(request):
     persona = None
     article = None
     try:
         persona = Persona.objects.get(id=1)
-        article = Article.objects.filter(owner=persona.id).orde_by('id').get(1)
+        article = Article.objects.order_by('id').filter(owner=persona.id)[0]
 
     except Persona.DoesNotExist:
         persona = Persona.objects.create("Pavel Garani")
-    return render_to_response('about_me/about_me.html',{'persona':persona,'menu':3,'about_me':article})
+    return render_to_response('about_me/about_me.html',{
+        'persona':persona,
+        'page_menu':3,
+        'about_me':article})
